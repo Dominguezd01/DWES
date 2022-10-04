ef9323279b60b97909267a702057f2940bd96ecb
Dominguez <migueldominguezroman01@gmail.com>
2022-10-04 14:19:41 +0200

    04/10/2022

--- UT2/Cosa de Arrays/Ejercicio1.php
+++ UT2/Cosa de Arrays/Ejercicio1.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,15 +8,54 @@
 </head>
 <body>
     <?php
-        $numeros = [0,-5,4,-56,3];
-        $cont = 0;
-        for($i = 0;$i<=sizeof($numeros);$i++){
+        $numeros = [2,-5,78,-56,3];
+        
+        function numerosNegativos($numeros){
+            $cont = 0;
+            for($i = 0;$i<sizeof($numeros);$i++){
 
-            if($numeros[$i] < 0 ){
-                $cont ++;
+                if($numeros[$i] < 0 ){
+                    $cont ++;
+                }
+            }
+            echo "Hay $cont valores negativos<br> ";
+        }
+        
+        function media($numeros){
+            $suma = 0;
+            $media = 0;
+            foreach($numeros as $valor){
+                $suma += $valor;
+                $media = $suma/sizeof($numeros);
+
+            }
+            echo "La media es: $media<br>";
+        }
+
+        function cuadradoDePosicion($numeros){
+            $cuadrados = false;
+            for($i=0; $i<sizeof($numeros);$i++){
+                if($i**2 == $numeros[$i]){
+                    echo "La poscion $i almacena un valor igual al cuadrado de si misma<br>";
+                    $cuadrados = true;
+                }
+            }
+            if(!$cuadrados){
+                echo "Ningun valor tiene el cuadrado de su indice almacenado<br>";
             }
         }
-        echo $cont;
+
+        function arrayInverso($numeros){
+            for($i = sizeof($numeros)-1;$i>=0;$i--){
+                echo "$numeros[$i] ";
+            } 
+        }
+
+        numerosNegativos($numeros);
+        media($numeros);
+        cuadradoDePosicion($numeros);
+        arrayInverso($numeros);
+
     ?>
 </body>
 </html>
