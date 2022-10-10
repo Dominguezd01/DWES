e3e7269efe203321f07ee552b92cf8858b57ef0e
Dominguez <migueldominguezroman01@gmail.com>
2022-10-10 09:55:07 +0200

    10/10/22

--- UT2 PARTE II/prueba.php
+++ UT2 PARTE II/prueba.php
@@ -8,6 +8,9 @@
 </head>
 <body>
 <?php
+
+
+
     $numeros = [2,4,5,1,3];
 /*
     if(in_array("3",$numeros, TRUE )){ //TRUE --> Tiene que coincidir valor y tipo
@@ -16,7 +19,7 @@
         echo "Valor no encontrado";
     }
 */
-
+/*
     sort($numeros);
     for($i = 0; $i<count($numeros); $i++){
         echo "$numeros[$i] ";
@@ -26,6 +29,44 @@
     for($i = 0; $i<count($numeros); $i++){
         echo "$numeros[$i] ";
     }
+    echo "<br>";
+    $modulosSegundo = [
+        "DWES" => 9,
+        "DWEC" => 8,
+        "DI" => 5,
+        "DA" => 4,
+        "EIE" => 3,
+        "FCT" => 6 
+    ];
+
+    foreach($modulosSegundo as $clave => $valor){
+        echo "$clave tiene $valor horas<br>";
+    }
+    echo "<br>";
+    krsort($modulosSegundo);
+
+    foreach($modulosSegundo as $clave => $valor){
+       echo "$clave tiene $valor horas<br>";
+    }
+*/
+/*
+    $numeros2 = [[2,3,1],[1,4,5],[7,10,20]];
+    foreach($numeros2 as list($n1,$n2,$n3)){
+        echo "$n1 ", "$n2 ", "$n3 ","<br>";
+    }
+
+    $colores = "Rojo-verde-azul-amarillo";
+
+    $array = explode("-",$colores );
+
+    foreach($array as $color){
+        echo "$color -->";
+    }
+    */
+    $persona = ["Pedro", "Gonzalez", "Lopez"];
+
+    $personaString = implode(" ", $persona);
+    echo $personaString;
 ?>
 </body>
 </html>
