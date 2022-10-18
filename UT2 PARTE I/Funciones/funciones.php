f920e3e596e5f475cac3d065a542808e48ec931d
Dominguez <migueldominguezroman01@gmail.com>
2022-10-18 13:24:42 +0200

    18/10/22

--- UT2 PARTE I/Funciones/funciones.php
+++ UT2 PARTE I/Funciones/funciones.php
@@ -35,4 +35,25 @@
     function mostrar ($a, $b, $c){
         echo $a, $b, $c, "<br>";
     }
+
+
+    function producto(&$resultado){
+        switch(func_num_args()){
+            case 1:
+                $resultado = 0;
+                break;
+
+            default: 
+                $parametros = func_get_args();
+                //echo var_dump($parametros);
+
+                $resultado = 1;
+
+                unset($parametros[0]);
+                echo var_dump($parametros); 
+                foreach($parametros as $valor){
+                    $resultado *= $valor;
+                }
+        }
+    }
 ?>
--- UT2 PARTE I/Funciones/pruebas_usuario.php
+++ UT2 PARTE I/Funciones/pruebas_usuario.php
@@ -1,5 +1,5 @@
 <?php
-    declare(strict_types = 1); // Strict mode en JS hay que ponerlo donde se invoca
+    declare(strict_types = 1); //Obliga a que los tipos coincidan
     require_once("./funciones.php");
     echo "El cubo es ",cubo(3), "<br>"; 
     echo "El cubo es ", cubo(NULL), "<br>";
@@ -9,4 +9,8 @@
     echo calcular($operacion,10,20),"<br>";
 
     mostrar(3,6,7);
+
+    producto($r, 2,3,4,5);
+
+    echo "El resultado es: $r <br>"
 ?>
