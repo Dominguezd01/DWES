c95d4b18bac4ac255ad8e4beaeabe2a5f182df30
Dominguez <migueldominguezroman01@gmail.com>
2022-11-22 14:18:45 +0100

    22/11/2022

--- UT4/PruebaErroresControlador/index.php
+++ UT4/PruebaErroresControlador/index.php
@@ -2,14 +2,20 @@
     set_error_handler("miControlador");
 
     $dividir =  5/0;
-
+    $cadena = "a";
     restore_error_handler();
+    if(!is_numeric($cadena)){
+        trigger_error("No numerico",E_USER_NOTICE);
+    }
 
     function miControlador ($nivel, $mensaje){
         switch($nivel){
             case E_WARNING: 
                 $mensaje = "No se puede dividir por cero";
                 echo "Error de tipo WARNING: $mensaje";
+                break;
+            case E_USER_NOTICE:
+                echo "Error de tipo Notice, $mensaje";
                 break;
             default:
                 echo "Error no identificado";
