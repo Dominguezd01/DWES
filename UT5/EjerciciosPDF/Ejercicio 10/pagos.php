6ee56322fc2e31f86f6112a5fc96441dc9f2d548
Dominguez <migueldominguezroman01@gmail.com>
2022-12-20 14:17:50 +0100

    20/12/2022

--- UT5/EjerciciosPDF/Ejercicio 10/pagos.php
+++ UT5/EjerciciosPDF/Ejercicio 10/pagos.php
@@ -15,7 +15,7 @@
     }
 </style>
 <body>
-    <form method="POST" id="formularioProductos">
+    <form method="POST" id="formularioProductos" action="">
             <input type="radio" name="producto" value="Platanos">Platanos<br>
             <input type="radio" name="producto" value="Manzanas">Manzanas<br>
             <input type="radio" name="producto" value="Peras">Peras<br><br>
@@ -27,14 +27,41 @@
 </html>
 <?php
     session_start();
-    var_dump($_POST);
+    echo "--- ESTO ES EL POST ---";
     var_dump($_COOKIE);
+    $arrayProductos = ["Platanos" =>0,
+                        "Manzanas" =>0,
+                        "Peras" => 0];
+
     if (!isset($_POST["enviar"])) {
         echo "Compra tus productos";
     }else{
         $producto = $_POST["producto"];
         $unidades = $_POST["numUds"];
-        setcookie($producto, $unidades , time() + 360);
-        var_dump($_COOKIE);
+        
+
+        //mostrarCarrito($producto, $unidades);
+        crearCarrito($producto, $unidades);
     }
+
+    function crearCarrito($producto, $uds){
+        if(!isset($_COOKIE[$producto])){
+            //echo "No esta creado";
+            setcookie($producto, $uds, time()+360);
+        }else{
+            setcookie($producto, $_COOKIE[$producto] +$uds, time()+360);
+        }
+        echo"--ESTO ES EL ARRAY DE PRODUCTOS--<br>";
+        //echo $arrayProductos[$producto];
+        var_dump($_COOKIE);
+        header("location: ./pagos.php");
+    }
+
+    function mostrarCarrito(){
+        echo 
+        <table>
+            <tr>Platanos<tr>
+        </table> 
+    }
+    //var_dump($arrayProductos);
 ?>
