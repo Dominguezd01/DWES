2262e9217dd5aff3b0c7d1f348e476bddcd50405
Dominguez <migueldominguezroman01@gmail.com>
2023-01-11 14:17:59 +0100

    11/01/2023

--- UT6/Peliculas/crearDatabase.sql
+++ UT6/Peliculas/crearDatabase.sql
@@ -14,6 +14,16 @@ CONSTRAINT pk_intervenciones PRIMARY KEY(id_actor, titulo),
 CONSTRAINT fk_id_actor FOREIGN KEY (id_actor) REFERENCES ACTOR (id_actor),
 CONSTRAINT fk_titulo FOREIGN KEY (titulo) REFERENCES PELICULAS (titulo)) ENGINE=”InnoDB”;
 
+CREATE TABLE cine(id_cine VARCHAR(15),nombre_c varchar(30),ciudad_c varchar(30),CONSTRAINT pk_cine PRIMARY KEY (id_cine)) ENGINE=”InnoDB”;
+
+ INSERT INTO cine
+ VALUES
+ ('CINE1','Odeon','Burgos'),
+ ('CINE2','Van Golem','Burgos'),
+ ('CINE3','CineSur','Sevilla'),
+ ('CINE4','Capitol','Madrid'),
+ ('CINE5','Conde Duque','Madrid');
+
 INSERT INTO actor (id_actor, nacionalidad, nombre_apellidos, sexo) VALUES(00001, "Frances", "Jacqueline Bisset", "f")
 INSERT INTO actor (id_actor, nacionalidad, nombre_apellidos, sexo) VALUES(00002, "Espanol", "Antonio Banderas", "m");
 INSERT INTO actor (id_actor, nacionalidad, nombre_apellidos, sexo) VALUES(00003, "Espanol", "Belen Rueda", "f");
@@ -27,11 +37,42 @@ VALUES
 ("Historia de un matrimonio", "drama", 2019),
 ("La piel que habito", "drama", 2019);
 
-(00002, "Dolor y gloria");
 
 INSERT INTO INTERVENCIONES
 VALUES
+(00002, "Dolor y gloria"),
 (00004, "Erase una vez... Hollywood"),
 (00005, "Historia de un matrimonio"),
 (00002, "La piel que habito");
 
+
+
+SELECT * FROM INTERVENCIONES WHERE genero="drama"
+
+
+
+
+CREATE TABLE proyectar(
+    titulo varchar(30),
+    id_cine varchar(30),
+    fecha_proy date
+) ENGINE="InnoDb";
+
+
+
+INSERT INTO proyectar VALUES
+('Dolor y gloria', 'CINE1', '2019-11-15'),
+('Dolor y gloria', 'CINE2', '2019-11-15'),
+('Dolor y gloria', 'CINE3', '2019-11-15'),
+('Dolor y gloria', 'CINE4', '2019-11-15'),
+('Dolor y gloria', 'CINE5', '2019-11-15'),
+('Erase una vez...Hollywood', 'CINE1', '2019-11-01'),
+('Erase una vez...Hollywood', 'CINE3', '2019-11-01'),
+('Erase una vez...Hollywood', 'CINE4', '2019-11-01'),
+('Historia de un matrimonio', 'CINE3', '2019-10-15'),
+('Historia de un matrimonio', 'CINE4', '2019-10-15'),
+('La piel que habito', 'CINE1', '2010-10-01'),
+('La piel que habito', 'CINE2', '2010-10-01'),
+('La piel que habito', 'CINE3', '2010-10-01'),
+('La piel que habito', 'CINE4', '2010-10-01'),
+('La piel que habito', 'CINE5', '2010-10-01');
---
+++ UT6/Peliculas/pruebaDB.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    :root{
+        color-scheme: dark;
+    }
+    body{
+        display: flex;
+        flex-wrap: wrap;
+        flex-flow: column wrap;
+        column-gap: 5em;
+        row-gap: 5em;
+        place-content: center;
+        background: rgb(74,73,73);
+        background: linear-gradient(90deg, rgba(74,73,73,1) 0%, rgba(36,27,27,1) 66%);
+    }
+    form{
+        display: flex;
+        margin-bottom: 1em;
+    }
+    .alumno{
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        display: grid;
+        background: rgb(74,73,73);
+        background: radial-gradient(circle, rgba(74,73,73,1) 0%, rgba(124,124,124,1) 66%);
+        border-radius: 15px;
+        padding: 1em;
+        place-items: center;
+    }
+</style>
+<body>
+        <form action="" method="POST" id="form">
+            <input type="text" placeholder="Nacionalidad" name="nacionalidad">
+
+            <input type="submit" name="enviar">
+        </form>
+
+        <div id="alumnos">
+
+        </div>
+</body>
+
+</html>
+<?php
+
+    $mysqli = new mysqli("localhost", "root", "", "peliculas", 3306);
+    
+    function sentenciaDB($mysqli){
+        $mysqli->real_query("Select * from alumno");
+        $sentencia = $mysqli->use_result();
+        var_dump($sentencia);
+        foreach($sentencia as $fila){
+            var_dump($fila);
+            mostrarUsuario($fila["nombre"], $fila["id_al"], $fila["id_curso"], $fila["edad"]);
+        }
+    }
+    
+
+    function mostrarUsuario($idActor, $nombre, $nacionalidad, $sexo){
+        $sexoCompleto ="";
+        if($sexo == "m"){
+            $sexoCompleto ="Masculino";
+            
+        }
+        if($sexo == "f"){
+            $sexoCompleto="Femenino";
+        }
+        
+        echo "<div class='alumno'>
+                <span> ID actor:$idActor</span>
+                <span> Nombre:$nombre</span>
+                <span> Nacionalidad:$nacionalidad</span>
+                <span> Sexo:$sexoCompleto</span>
+              </div>";
+    }
+
+    if(isset($_POST["enviar"])){insertarUsuario($mysqli);}
+
+    function insertarUsuario($mysqli){
+
+
+        $mysqli ->real_query("SELECT * FROM ACTOR WHERE nacionalidad= '$_POST[nacionalidad]'");
+        $sentencia = $mysqli->use_result();
+        //var_dump($sentencia);
+        foreach($sentencia as $fila){
+            //var_dump($fila);
+            mostrarUsuario($fila["nacionalidad"], $fila["nombre_apellidos"], $fila["nacionalidad"], $fila["sexo"]);
+        }
+        
+        
+    }
+
+    //sentenciaDB($mysqli);
